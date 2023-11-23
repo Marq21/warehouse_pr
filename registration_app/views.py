c514249115461cd546c05e42c09845dff9c54b59
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2023-11-23 13:13:53 +0300

    Added auth by email| Added status messages

--- registration_app/views.py
+++ registration_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
-from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -35,6 +37,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '
+                             'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
