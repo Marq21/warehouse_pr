75d23710ea8e7333a1dd2428153b7e94e78fc23d
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2023-12-26 15:20:27 +0300

    added category & user serializers

--- store_api/serializers.py
+++ store_api/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework.serializers import ModelSerializer
 from catalog.models import Nomenclature, Category
+from django.contrib.auth import get_user_model
 
-class NomenclatureSerializer(ModelSerializer):
+
+class UserSerializer(ModelSerializer):
     class Meta:
-        model = Nomenclature
-        fields = '__all__'
+        model = get_user_model()
+        fields = ['id', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name',
+                  'email', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions']
 
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
-        
+
+
+class NomenclatureSerializer(ModelSerializer):
+    user = UserSerializer()
+    category = CategorySerializer()
+
+    class Meta:
+        model = Nomenclature
+        fields = ['name', 'cost', 'weight_or_piece',
+                  'barcode', 'slug', 'user', 'country_made', 'category']
