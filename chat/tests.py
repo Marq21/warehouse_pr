a1f30f0d2d85de7a25ea7cddd1d0234ca15dc1c5
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-03-01 14:29:25 +0300

    test chat view by status code

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ChatTest(TestCase):
+
+    def test_status_code(self):
+        user = User.objects.create_user(
+            'john', 'lennon@thebeatles.com', 'johnpassword')
+        self.user = user
+        self.client.login(username='john', password='johnpassword')
+        resp = self.client.get("/chat/room/")
+        self.assertEqual(resp.status_code, 200)
